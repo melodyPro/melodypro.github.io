2d7ef771e54b8c0ac17d2d192528c1a9dfb2970c
melody-pro <melody-pro@hotmail.com>
2017-06-25 21:37:08 +0800

    Site updated: 2017-06-25 21:37:06

---
+++ JavaScript/js/js-note.js
@@ -0,0 +1,56 @@
+$(function(){
+    var dataType = function(){
+        var width=600, height=300;
+        var cluster=d3.layout.cluster()     //����һ����Ⱥͼ����
+            .size([width-300, height]);
+
+        var svg=d3.select(".data-type-svg").append("svg")
+            .attr("width", width)
+            .attr("height", height)
+            .append("g")
+            .attr("transform", "translate(150,0)");
+
+        var diagonal=d3.svg.diagonal()      //����һ������������
+            .projection(function (d) {      //projection ��任��������x��y�����꣬��Ĭ�������ͼ�α�Ϊ����
+                return [d.y, d.x];
+            });
+
+        d3.json("/JavaScript/js/data-type.json", function (error,data) {
+            var nodes=cluster.nodes(data);
+            var links=cluster.links(nodes);
+
+            var link=svg.selectAll(".link")
+                .data(links)
+                .enter()
+                .append("path")
+                .attr("class", "link")
+                .attr("d", diagonal);
+
+            var node=svg.selectAll(".node")
+                .data(nodes)
+                .enter()
+                .append("g")
+                .attr("class", "node")
+                .attr("transform",function(d){
+                    return "translate(" + d.y + "," + d.x + ")";
+                });
+
+            node.append("circle")
+                .attr("r",5);
+
+            node.append("text")
+                .attr("dx",function(d){
+                    return d.children ? -8 : 8;
+                })
+                .attr("dy",3)
+                .style("text-anchor",function(d){
+                    return d.children ? "end" : "start";
+                })
+                .text(function(d){
+                    return d.name;
+                });
+        })
+    };
+
+    dataType();
+});
